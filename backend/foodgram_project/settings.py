6ef454c2445d428c61b547c6a3a7889ef570832a
Алексей Нечепуренко <nec-alesha30@yandex.ru>
2024-03-24 22:54:28 +0300

    Third review

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['foodgrambrandnew.hopto.org', 'localhost']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
